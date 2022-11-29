c1e83bc2348d6e3affe12e1675d6dffbb936daa5
shanku45 <shankunatra678@gmail.com>
2022-11-29 14:40:43 +0530

    junit first commit

---
+++ src/test/java/junitjen/jenjunit/AppTest.java
@@ -0,0 +1,23 @@
+package junitjen.jenjunit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+	@Test
+    public void test()
+    {
+    	App a= new App();
+    	int actual=a.add(17,15);
+    	int expected=30;
+        assertEquals(expected,actual);
+    }
+}
